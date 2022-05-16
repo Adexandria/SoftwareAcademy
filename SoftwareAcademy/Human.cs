dc39e31beeba8354a2bfeb38632c656b0ae94317
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-05-16 17:51:33 +0100

    added human model

---
+++ SoftwareAcademy/Human.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftwareAcademy
+{
+    public class Human
+    {
+        public Human(AcademyProgramme academyProgramme)
+        {
+            this.AcademyProgramme = academyProgramme;
+        }
+        public AcademyProgramme AcademyProgramme { get; set; }
+        
+    }
+}
