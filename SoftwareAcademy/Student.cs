ded6642de6722e464809e78d1e08814d52f040bb
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-05-16 17:53:23 +0100

    student model

---
+++ SoftwareAcademy/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftwareAcademy
+{
+    public class Student : Human
+    {
+      public Student(AcademyProgramme academyProgramme) :base(academyProgramme)
+      {
+
+      }
+    }
+}
