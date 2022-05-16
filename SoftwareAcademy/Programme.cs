7fe7c3d5fb861340bf1ba9ab8eac2315fe3dbc54
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-05-16 17:51:45 +0100

    added programme

--- SoftwareAcademy/Programme.cs
+++ SoftwareAcademy/Programme.cs
@@ -4,9 +4,44 @@ using System.Text;
 
 namespace SoftwareAcademy
 {
-    public enum Programme
+    public class Programme
     {
-        Frontend,
-        Backend
+        public Programme(AcademyProgramme academyProgramme)
+        {
+            this.AcademyProgramme = academyProgramme;
+            if(this.AcademyProgramme == AcademyProgramme.Frontend)
+            {
+                this.Days = GetFrontendDays();
+            }
+            else
+            {
+                this.Days = GetBackendDays();
+            }
+        }
+        public AcademyProgramme AcademyProgramme { get; set; }
+        public List<Student> Students { get; set; }
+        public List<Facilitator> Facilitators { get; set; }
+        public List<Day> Days { get; set; }
+       
+        private List<Day> GetFrontendDays()
+        {
+            List<Day> frontendDays = new List<Day>
+            {
+                Day.Tuesday,
+                Day.Wednesday,
+                Day.Thursday
+            };
+            return frontendDays;
+        }
+        private List<Day> GetBackendDays()
+        {
+            List<Day> backendDays = new List<Day>
+            {
+                Day.Monday,
+                Day.Tuesday,
+                Day.Thursday
+            };
+            return backendDays;
+        }
     }
 }
