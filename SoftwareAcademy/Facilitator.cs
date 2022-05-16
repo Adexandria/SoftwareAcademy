27b85879de59d619571615e0f5d7607540bd2900
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-05-16 17:51:16 +0100

    added facilitator task

---
+++ SoftwareAcademy/Facilitator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftwareAcademy
+{
+    public class Facilitator : Human
+    {
+        public Facilitator(AcademyProgramme academyProgramme) : base(academyProgramme)
+        {
+
+        }
+    }
+}
